b2726b9469cf3781cda408298d8ef15fcd629310
Yannick Lohse <contact@yannick-lohse.fr>
2016-10-01 19:30:45 +0200

    Simple StringBuilder port, see #30

---
+++ engine/StringBuilder.js
@@ -0,0 +1,26 @@
+export class StringBuilder{
+	constructor(str){
+		str = (typeof str !== 'undefined') ? str.toString() : '';
+		this._string = str;
+	}
+	get Length(){
+		return this._string.length;
+	}
+	Append(str){
+		this._string += str;
+	}
+	AppendLine(str){
+		if (typeof str !== 'undefined') this.Append(str);
+		this._string += "\n";
+	}
+	AppendFormat(format){
+		//taken from http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+		var args = Array.prototype.slice.call(arguments, 1);
+		return format.replace(/{(\d+)}/g, function(match, number){
+			return typeof args[number] != 'undefined' ? args[number] : match;
+		});
+	}
+	toString(){
+		return this._string;
+	}
+}
