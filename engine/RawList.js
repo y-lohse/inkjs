14192587682c091a8a14b14c7919263bca2f0e6a
Yannick Lohse <contact@yannick-lohse.fr>
2017-01-29 23:21:29 +0100

    Generate hashcode for null RawListItems

--- engine/RawList.js
+++ engine/RawList.js
@@ -38,7 +38,7 @@ export class RawListItem{
 	toString(){
 		//WARNING: experimental. RawListItem are structs and are used as keys inside hashes. In js, we can't use an object as a key, as the key needs to be a string. C# gets around that with the internal GetHashCode, and the js equivalent to that is toString. So here, toString acts as C#'s GetHashCode
 		var originCode = '0';
-		var itemCode = this.itemName.toString();
+		var itemCode = (this.itemName) ? this.itemName.toString() : 'null';
 		if (this.originName != null)
 			originCode = this.originName.toString();
 		
