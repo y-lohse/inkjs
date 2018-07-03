758d0c65121ed566380c05695dd09f460848d819
Frédéric Maquin <maquin.frederic@gmail.com>
2018-07-03 13:47:45 +1000

    Add missing  - fix typos and oversights

--- engine/SearchResult.js
+++ engine/SearchResult.js
@@ -1,9 +1,3 @@
-// When looking up content within the story (e.g. in Container.ContentAtPath),
-// the result is generally found, but if the story is modified, then when loading
-// up an old save state, then some old paths may still exist. In this case we
-// try to recover by finding an approximate result by working up the story hierarchy
-// in the path to find the closest valid container. Instead of crashing horribly,
-// we might see some slight oddness in the content, but hopefully it recovers!
 export class SearchResult{
 	constructor(){
 		this.obj;
@@ -11,10 +5,10 @@ export class SearchResult{
 	}
 
 	get correctObject(){
-		return this.approximate ? null : obj;
+		return this.approximate ? null : this.obj;
 	}
 
 	get container(){
-		return obj;
+		return (this.obj instanceof Container) ? this.obj : null;
 	}
 }
