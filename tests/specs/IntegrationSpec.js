1ab1ef4f6e31e79e5b04e8e2eb251a73bc315d71
Yannick Lohse <contact@yannick-lohse.fr>
2018-07-14 16:44:40 +0200

    fix: new resetCallStack behavior

--- tests/specs/IntegrationSpec.js
+++ tests/specs/IntegrationSpec.js
@@ -51,18 +51,18 @@ describe('Integration', function(){
     story.ObserveVariable('observedVar2', spy2);
     story.ObserveVariable('observedVar1', commonSpy);
     story.ObserveVariable('observedVar2', commonSpy);
-    
+
     expect(story.Continue()).toEqual('declared\n');
-    
+
     expect(story.variablesState['observedVar1']).toEqual(1);
     expect(story.variablesState['observedVar2']).toEqual(2);
     expect(spy1).toHaveBeenCalledTimes(0);
     expect(commonSpy).toHaveBeenCalledWith('observedVar1', 2);
 //    expect(spy2).toHaveBeenCalledTimes(0);
 //    expect(commonSpy).toHaveBeenCalledTimes(0);
-    
+
 //    expect(story.Continue()).toEqual('mutated 1\n');
-    
+
 //    expect(story.variablesState['observedVar1']).toEqual(3);
 //    expect(story.variablesState['observedVar2']).toEqual(2);
 //    expect(spy1).toHaveBeenCalledTimes(1);
@@ -70,21 +70,21 @@ describe('Integration', function(){
 //    expect(spy2).toHaveBeenCalledTimes(0);
 //    expect(commonSpy).toHaveBeenCalledTimes(1);
 //    expect(commonSpy).toHaveBeenCalledWith('observedVar1', 3);
-//    
+//
 //    expect(story.Continue()).toEqual('mutated 2\n');
-//    
+//
 //    expect(story.variablesState['observedVar1']).toEqual(4);
 //    expect(story.variablesState['observedVar2']).toEqual(5);
-//    
+//
 //    expect(spy1).toHaveBeenCalledTimes(2);
 //    expect(spy1).toHaveBeenCalledWith('observedVar1', 4);
 //    expect(spy1).toHaveBeenCalledTimes(1);
 //    expect(spy1).toHaveBeenCalledWith('observedVar2', 5);
   });
-  
+
   it('should increment the read count on each visit', function(){
     expect(story.state.VisitCountAtPathString('integration.visit_count')).toEqual(0);
-    
+
     for (let i = 0; i < 10; ++i){
       story.ChoosePathString('integration.visit_count');
       expect(story.Continue()).toEqual('visited\n');
@@ -94,32 +94,41 @@ describe('Integration', function(){
       story.Continue();
     }
   });
-  
-  it('should not increment the read count as long as the knot is not left', function(){
+
+  it('should increment the read count when the callstack is reset', function(){
     expect(story.state.VisitCountAtPathString('integration.visit_count')).toEqual(0);
-    
+
     for (let i = 0; i < 10; ++i){
       story.ChoosePathString('integration.visit_count');
       expect(story.Continue()).toEqual('visited\n');
-      expect(story.state.VisitCountAtPathString('integration.visit_count')).toEqual(1);
-      // here we don't leave the knot, so we never enter it again
+      expect(story.state.VisitCountAtPathString('integration.visit_count')).toEqual(i + 1);
     }
   });
-  
+
+  it('should not increment the read count when the callstack is not reset', function(){
+    expect(story.state.VisitCountAtPathString('integration.visit_count')).toEqual(0);
+
+    for (let i = 0; i < 10; ++i){
+      story.ChoosePathString('integration.visit_count', false);
+      expect(story.Continue()).toEqual('visited\n');
+      expect(story.state.VisitCountAtPathString('integration.visit_count')).toEqual(1);
+    }
+  });
+
   it('should call ink functions', function(){
     expect(story.EvaluateFunction('fn_with_return')).toEqual('returned');
     expect(story.EvaluateFunction('fn_without_return')).toBeNull();
     expect(story.EvaluateFunction('fn_print')).toBeNull();
     expect(story.EvaluateFunction('fn_calls_other')).toEqual('nested function called');
   });
-  
+
   it('should call ink functions with params', function(){
     expect(story.EvaluateFunction('fn_params', ['a', 'b'])).toEqual('was a');
     expect(story.EvaluateFunction('fn_echo', ['string'])).toEqual('string');
     expect(story.EvaluateFunction('fn_echo', [5])).toEqual(5);
     expect(story.EvaluateFunction('fn_echo', [5.3])).toEqual(5.3);
   });
-  
+
   it('should return output and return value from ink function calls', function(){
     expect(story.EvaluateFunction('fn_print', [], true)).toEqual({
       returned: null,
@@ -138,7 +147,7 @@ describe('Integration', function(){
       output: '5.3\n'
     });
   });
-  
+
   it('should call external functions', function(){
     story.allowExternalFunctionFallbacks = false;
     story.ChoosePathString('integration.external');
@@ -146,30 +155,30 @@ describe('Integration', function(){
       return a;
     }).and.callThrough();
     story.BindExternalFunction('fn_ext', externalSpy);
-    
+
     expect(story.ContinueMaximally()).toEqual('1\n1.1\na\na\n');
     expect(externalSpy).toHaveBeenCalledWith(1, 2, 3);
     expect(externalSpy).toHaveBeenCalledWith(1.1, 2.2, 3.3);
     expect(externalSpy).toHaveBeenCalledWith('a', 'b', 'c');
     expect(externalSpy).toHaveBeenCalledWith('a', 1, 2.2);
   });
-  
+
   it('should return a visit count', function(){
     expect(story.state.VisitCountAtPathString('game_queries.turnssince')).toEqual(0);
-    
+
     story.ChoosePathString('game_queries.turnssince');
     story.Continue();
     expect(story.state.VisitCountAtPathString('game_queries.turnssince')).toEqual(1);
-    
+
     story.ChoosePathString('game_queries.turnssince_1');
     story.Continue();
     story.ChoosePathString('game_queries.turnssince');
     story.Continue();
     expect(story.state.VisitCountAtPathString('game_queries.turnssince')).toEqual(2);
   });
-  
+
   describe('Saving and Loading', function(){
-    
+
     it('should continue the story', function(){
       story.ChoosePathString('saveload');
       expect(story.Continue()).toEqual('a bit of content\n');
@@ -177,40 +186,40 @@ describe('Integration', function(){
       story.state.LoadJson(save);
       expect(story.Continue()).toEqual('the next bit\n');
     });
-    
+
     it('should restore a choice point', function(){
       story.ChoosePathString('saveload.choicepoint');
       story.Continue();
       expect(story.currentChoices.length).toEqual(2);
       expect(story.currentChoices[0].text).toEqual('choice 1');
       expect(story.currentChoices[1].text).toEqual('choice 2');
-      
+
       const save = story.state.ToJson();
       story.state.LoadJson(save);
-      
+
       expect(story.currentChoices.length).toEqual(2);
       expect(story.currentChoices[0].text).toEqual('choice 1');
       expect(story.currentChoices[1].text).toEqual('choice 2');
     });
-    
+
   });
-  
+
   describe('debug tools', function(){
-    
+
     it('should return a string of hierarchy', function(){
       expect(story.BuildStringOfHierarchy()).toBeDefined();
     });
-    
+
   });
-  
+
   describe('Exported classes', function(){
     it('should expose the Story class', function(){
       expect(testsUtils.inkjs.Story).toBeDefined();
     });
-    
+
     it('should expose the InkList class', function(){
       expect(testsUtils.inkjs.InkList).toBeDefined();
     });
   })
-  
+
 });
