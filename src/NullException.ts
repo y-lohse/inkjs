5bc2082eac4f96cc9fcc96a7c9356a3bb3234bdd
Frédéric Maquin <maquin.frederic@gmail.com>
2018-07-16 22:28:15 +1000

    feat: convert InkList to TypeScript make it inherit Map

---
+++ src/NullException.ts
@@ -0,0 +1,19 @@
+/**
+ * In the original C# code, a SystemException would be thrown when passing
+ * null to methods expected a valid instance. Javascript has no such
+ * concept, but TypeScript will not allow `null` to be passed to methods
+ * explicitely requiring a valid type.
+ *
+ * Whenever TypeScript complain about the possibility of a `null` value,
+ * check the offending value and it it's null, throw this exception using
+ * `throwNullException(name: string)`.
+ */
+export class NullException extends Error{}
+
+/**
+ * Throw a NullException.
+ * @param name a short description of the offending value (often its name within the code).
+ */
+export function throwNullException(name: string) {
+	throw new NullException(`${name} is null or undefined`);
+}
