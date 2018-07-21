16948f954eec8011aad0dcdf3f59c578828a30be
Frédéric Maquin <maquin.frederic@gmail.com>
2018-07-21 22:23:10 +1000

    feat: convert Divert and update related dependencies

--- src/Divert.ts
+++ src/Divert.ts
@@ -4,32 +4,13 @@ import {StringBuilder} from './StringBuilder';
 import {InkObject} from './Object';
 import {Pointer} from './Pointer';
 import {Container} from './Container';
+import {throwNullException} from './NullException';
 
 export class Divert extends InkObject{
-	constructor(stackPushType){
-		super();
-		this._targetPath;
-		this._targetPointer;
-
-		this.variableDivertName;
-		this.pushesToStack;
-		this.stackPushType;
-
-		this.isExternal;
-		this.isConditional;
-		this.externalArgs;
-
-		//actual constructor
-		this.pushesToStack = false;
-		if (stackPushType){
-			this.pushesToStack = true;
-			this.stackPushType = stackPushType;
-		}
-	}
 	get targetPath(){
 		// Resolve any relative paths to global ones as we come across them
 		if (this._targetPath != null && this._targetPath.isRelative) {
-			var targetObj = this.targetPointer.Resolve();
+			let targetObj = this.targetPointer.Resolve();
 			if (targetObj) {
 				this._targetPath = targetObj.path;
 			}
@@ -37,15 +18,22 @@ export class Divert extends InkObject{
 
 		return this._targetPath;
 	}
-	set targetPath(value){
+	set targetPath(value: Path | null){
 		this._targetPath = value;
 		this._targetPointer = Pointer.Null;
 	}
+
+	public _targetPath: Path | null = null;
+
 	get targetPointer(){
 		if (this._targetPointer.isNull) {
-			var targetObj = this.ResolvePath(this._targetPath).obj;
+			let targetObj = this.ResolvePath(this._targetPath).obj;
+
+			if (this._targetPath === null) return throwNullException('this._targetPath');
+			if (this._targetPath.lastComponent === null) return throwNullException('this._targetPath.lastComponent');
 
 			if (this._targetPath.lastComponent.isIndex) {
+				if (targetObj === null) return throwNullException('targetObj');
 				this._targetPointer.container = (targetObj.parent instanceof Container) ? targetObj.parent : null;
 				this._targetPointer.index = this._targetPath.lastComponent.index;
 			} else {
@@ -55,74 +43,97 @@ export class Divert extends InkObject{
 
 		return this._targetPointer.copy();
 	}
+
+	public _targetPointer: Pointer = Pointer.Null;
+
 	get targetPathString(){
 		if (this.targetPath == null)
 			return null;
 
 		return this.CompactPathString(this.targetPath);
 	}
-	set targetPathString(value){
+	set targetPathString(value: string | null){
 		if (value == null) {
 			this.targetPath = null;
 		} else {
 			this.targetPath = new Path(value);
 		}
 	}
+
+	public variableDivertName: string | null = null;
 	get hasVariableTarget(){
 		return this.variableDivertName != null;
 	}
 
-	Equals(obj){
-//		var otherDivert = obj as Divert;
-		var otherDivert = obj;
+	public pushesToStack: boolean = false;
+	public stackPushType: PushPopType = 0;
+
+	public isExternal: boolean = false;
+	public externalArgs: number = 0;
+
+	public isConditional: boolean = false;
+
+	constructor(stackPushType?: PushPopType){
+		super();
+		this.pushesToStack = false;
+
+		if (typeof stackPushType !== 'undefined') {
+			this.pushesToStack = true;
+			this.stackPushType = stackPushType;
+		}
+	}
+
+	public Equals(obj: Divert | null){
+		let otherDivert = obj;
 		if (otherDivert instanceof Divert) {
 			if (this.hasVariableTarget == otherDivert.hasVariableTarget) {
 				if (this.hasVariableTarget) {
 					return this.variableDivertName == otherDivert.variableDivertName;
 				} else {
+					if (this.targetPath === null) return throwNullException('this.targetPath');
 					return this.targetPath.Equals(otherDivert.targetPath);
 				}
 			}
 		}
 		return false;
 	}
-	toString(){
+
+	public toString(){
 		if (this.hasVariableTarget) {
-			return "Divert(variable: " + this.variableDivertName + ")";
+			return 'Divert(variable: ' + this.variableDivertName + ')';
 		}
 		else if (this.targetPath == null) {
-			return "Divert(null)";
+			return 'Divert(null)';
 		} else {
 
-			var sb = new StringBuilder;
+			let sb = new StringBuilder();
 
-			var targetStr = this.targetPath.toString();
-//			int? targetLineNum = DebugLineNumberOfPath (targetPath);
-			var targetLineNum = null;
+			let targetStr = this.targetPath.toString();
+			// int? targetLineNum = DebugLineNumberOfPath (targetPath);
+			let targetLineNum = null;
 			if (targetLineNum != null) {
-				targetStr = "line " + targetLineNum;
+				targetStr = 'line ' + targetLineNum;
 			}
 
-			sb.Append("Divert");
+			sb.Append('Divert');
 
-			if (this.isConditional) {
-				sb.Append("?");
-			}
+			if (this.isConditional)
+				sb.Append('?');
 
 			if (this.pushesToStack) {
 				if (this.stackPushType == PushPopType.Function) {
-					sb.Append(" function");
+					sb.Append(' function');
 				} else {
-					sb.Append(" tunnel");
+					sb.Append(' tunnel');
 				}
 			}
 
-			sb.Append(" -> ");
+			sb.Append(' -> ');
 			sb.Append(this.targetPathString);
 
-			sb.Append(" (");
+			sb.Append(' (');
 			sb.Append(targetStr);
-			sb.Append(")");
+			sb.Append(')');
 
 			return sb.toString();
 		}
