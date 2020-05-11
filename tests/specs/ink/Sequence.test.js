ac75daea3d3c23d2532fa10f89a96c817ed3176f
Frédéric Maquin <frederic@maquin.email>
2020-05-11 23:25:20 +0200

    tests: add sequences test cases

---
+++ tests/specs/ink/Sequence.test.js
@@ -0,0 +1,47 @@
+var testsUtils = require('../../common.js');
+
+describe('Sequences', () => {
+
+	var story;
+
+	function loadStory(name) {
+		story = testsUtils.loadInkFile(name, "sequences");
+		story.allowExternalFunctionFallbacks = true;
+	}
+
+	beforeEach(() => {
+		story = undefined;
+	});
+
+	test('blanks in inline sequences', () => {
+		loadStory("blanks_in_inline_sequences");
+		expect(story.ContinueMaximally()).toBe("1. a\n2.\n3. b\n4. b\n---\n1.\n2. a\n3. a\n---\n1. a\n2.\n3.\n---\n1.\n2.\n3.\n");
+	});
+
+	test('blanks in inline sequences', () => {
+		loadStory("empty_sequence_content");
+		expect(story.ContinueMaximally()).toBe("Wait for it....\nSurprise!\nDone.\n");
+	});
+
+	test('gather read count with initial sequence', () => {
+		loadStory("gather_read_count_with_initial_sequence");
+		expect(story.Continue()).toBe("seen test\n");
+	});
+
+	test('leading newline multiline sequence', () => {
+		loadStory("leading_newline_multiline_sequence");
+		expect(story.Continue()).toBe("a line after an empty line\n");
+	});
+
+	test('shuffle stack muddying', () => {
+		loadStory("shuffle_stack_muddying");
+
+		story.Continue();
+
+		expect(story.currentChoices.length).toBe(2);
+	});
+
+	xtest('all sequence type', () => { // Only available in 0.9.0
+		loadStory("all_sequence_types");
+	});
+});
