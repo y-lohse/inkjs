628fa18e1a20577c03a1d50e51e98c3d6f071e7e
Frédéric Maquin <frederic@maquin.email>
2020-05-12 13:16:30 +0200

    tests: add newlines test cases

---
+++ tests/specs/ink/Newlines.test.js
@@ -0,0 +1,48 @@
+var testsUtils = require('../../common.js');
+
+describe('Newlines', () => {
+
+	var story;
+
+	function loadStory(name) {
+		story = testsUtils.loadInkFile(name, "newlines");
+	}
+
+	beforeEach(() => {
+		story = undefined;
+	});
+
+	test('newline at start of multiline conditional', () => {
+		loadStory("newline_at_start_of_multiline_conditional");
+
+		expect(story.ContinueMaximally()).toBe("X\nx\n");
+	});
+
+	test('newline consistency', () => {
+		loadStory("newline_consistency_1");
+		expect(story.ContinueMaximally()).toBe("hello world\n");
+
+		loadStory("newline_consistency_2");
+		story.Continue();
+		story.ChooseChoiceIndex(0);
+		expect(story.ContinueMaximally()).toBe("hello world\n");
+
+		loadStory("newline_consistency_3");
+		story.Continue();
+		story.ChooseChoiceIndex(0);
+		expect(story.ContinueMaximally()).toBe("hello\nworld\n");
+	});
+
+	test('newlines trimming with func external fallback', () => {
+		loadStory("newlines_trimming_with_func_external_fallback");
+		story.allowExternalFunctionFallbacks = true;
+
+		expect(story.ContinueMaximally()).toBe("Phrase 1\nPhrase 2\n");
+	});
+
+	test('newlines trimming with string eval', () => {
+		loadStory("newlines_with_string_eval");
+
+		expect(story.ContinueMaximally()).toBe("A\nB\nA\n3\nB\n");
+	});
+});
