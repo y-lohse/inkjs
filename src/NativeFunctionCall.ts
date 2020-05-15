32e014a9865f294a57d89a045344086d91d25766
Yannick Lohse <contact@yannick-lohse.fr>
2020-05-15 12:53:00 +0200

    refactor: Don't use Object.keys

--- src/NativeFunctionCall.ts
+++ src/NativeFunctionCall.ts
@@ -129,7 +129,7 @@ export class NativeFunctionCall extends InkObject{
 			if (this._operationFuncs === null) return throwNullException('NativeFunctionCall._operationFuncs');
 			let opForTypeObj = this._operationFuncs.get(valType);
 			if (!opForTypeObj) {
-        const key = Object.keys(ValueType)[valType];
+        const key = ValueType[valType];
 				throw new StoryException('Cannot perform operation '+this.name+' on '+key);
 			}
 
@@ -180,7 +180,7 @@ export class NativeFunctionCall extends InkObject{
 		if (v1.valueType == ValueType.List && v2.valueType == ValueType.List)
 			return this.CallType<InkList>([v1, v2]);
 
-		throw new StoryException('Can not call use ' + this.name + ' operation on ' + v1.valueType + ' and ' + v2.valueType);
+		throw new StoryException('Can not call use ' + this.name + ' operation on ' + ValueType[v1.valueType] + ' and ' + ValueType[v2.valueType]);
 	}
 
 	public CallListIncrementOperation(listIntParams: InkObject[]){
@@ -255,7 +255,7 @@ export class NativeFunctionCall extends InkObject{
 					} else
 						throw new StoryException('Could not find List item with the value ' + intVal + ' in ' + list.name);
 				} else {
-          const key = Object.keys(ValueType)[val.valType];
+          const key = ValueType[val.valueType];
           throw new StoryException('Cannot mix Lists and ' + key + ' values in this operation');
         }
 			}
