5967eaf14dd00ecad8b27dcfd3ce859daa6eb4f6
Frédéric Maquin <frederic@maquin.email>
2021-03-13 21:04:39 +0100

    feat: migrate DebugMetadata.ts (#855)

--- src/engine/DebugMetadata.ts
+++ src/engine/DebugMetadata.ts
@@ -1,8 +1,47 @@
 export class DebugMetadata {
   public startLineNumber: number = 0;
   public endLineNumber: number = 0;
+  public startCharacterNumber: number = 0;
+  public endCharacterNumber: number = 0;
   public fileName: string | null = null;
   public sourceName: string | null = null;
+
+  public Merge(dm: DebugMetadata) {
+    let newDebugMetadata = new DebugMetadata();
+
+    newDebugMetadata.fileName = this.fileName;
+    newDebugMetadata.sourceName = this.sourceName;
+
+    if (this.startLineNumber < dm.startLineNumber) {
+      newDebugMetadata.startLineNumber = this.startLineNumber;
+      newDebugMetadata.startCharacterNumber = this.startCharacterNumber;
+    } else if (this.startLineNumber > dm.startLineNumber) {
+      newDebugMetadata.startLineNumber = dm.startLineNumber;
+      newDebugMetadata.startCharacterNumber = dm.startCharacterNumber;
+    } else {
+      newDebugMetadata.startLineNumber = this.startLineNumber;
+      newDebugMetadata.startCharacterNumber = Math.min(
+        this.startCharacterNumber,
+        dm.startCharacterNumber
+      );
+    }
+
+    if (this.endLineNumber > dm.endLineNumber) {
+      newDebugMetadata.endLineNumber = this.endLineNumber;
+      newDebugMetadata.endCharacterNumber = this.endCharacterNumber;
+    } else if (this.endLineNumber < dm.endLineNumber) {
+      newDebugMetadata.endLineNumber = dm.endLineNumber;
+      newDebugMetadata.endCharacterNumber = dm.endCharacterNumber;
+    } else {
+      newDebugMetadata.endLineNumber = this.endLineNumber;
+      newDebugMetadata.endCharacterNumber = Math.max(
+        this.endCharacterNumber,
+        dm.endCharacterNumber
+      );
+    }
+
+    return newDebugMetadata;
+  }
 
   public toString() {
     if (this.fileName !== null) {
