136c5953d1d2eccd7a709e92ce4ff27da5ea60d4
Ju / smwhr <ju@smwhr.net>
2022-02-07 15:33:55 +0100

    add Identifier.ts d213cda4ffa03a447b507e1d395ca3b80ea3b131

---
+++ src/compiler/Parser/ParsedHierarchy/Identifier.ts
@@ -0,0 +1,18 @@
+import { DebugMetadata } from "../../../engine/DebugMetadata";
+
+class Identifier {
+    public name?: string;
+    public debugMetadata: DebugMetadata|null = null;
+
+    get toString(): string{
+        return this.name || 'undefined identifer';
+    }
+
+    constructor(name: string){
+        this.name = name;
+    }
+
+    public static Done() : Identifier{
+        return new Identifier("DONE");
+    }
+}
