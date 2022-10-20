53b1b06ae16d7874d5d0a1d522e13a429b97d3e7
Ju / smwhr <ju@smwhr.net>
2022-10-20 12:06:59 +0200

    compiler/ParsedHierarchy/Tag.cs

--- src/compiler/Parser/ParsedHierarchy/Tag.ts
+++ src/compiler/Parser/ParsedHierarchy/Tag.ts
@@ -1,11 +1,33 @@
-import { Tag as RuntimeTag } from "../../../engine/Tag";
-import { Wrap } from "./Wrap";
+import { ParsedObject } from "./Object";
+import { ControlCommand } from "../../../engine/ControlCommand";
+import { InkObject as RuntimeObject } from "../../../engine/Object";
 
-export class Tag extends Wrap<RuntimeTag> {
-  constructor(tag: RuntimeTag) {
-    super(tag);
+export class Tag extends ParsedObject {
+
+  public isStart: boolean;
+  public inChoice: boolean;
+
+  constructor(isStart: boolean, inChoice: boolean = false) {
+    super();
+    this.isStart = isStart;
+    this.inChoice = inChoice;
   }
   get typeName(): string {
     return "Tag";
+  }
+  public readonly GenerateRuntimeObject = (): RuntimeObject => {
+    if(this.isStart){
+      return ControlCommand.BeginTag()
+    }else{
+      return ControlCommand.EndTag()
+    }
+  }
+
+  public readonly toString = () => {
+    if(this.isStart){
+      return "#StartTag";
+    }else{
+      return "#EndTag";
+    }
   }
 }
