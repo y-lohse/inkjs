b0b3e71eda5117dd3df50840fb36fa6a672a8486
Martin Crawford <martin.crawford@wealthsimple.com>
2023-07-24 16:32:11 -0400

    fix: typescript definitions broken

--- ink.d.ts
+++ ink.d.ts
@@ -1,18 +1,5 @@
-import { Story, InkList } from './engine/Story'
-import { Compiler } from './compiler/Compiler'
-import { CompilerOptions } from './compiler/CompilerOptions'
-import { PosixFileHandler } from './compiler/FileHandler/PosixFileHandler'
-import { JsonFileHandler } from './compiler/FileHandler/JsonFileHandler'
-
-declare interface Inkjs {
-    Story: typeof Story
-    InkList: typeof InkList
-    Compiler: typeof Compiler
-    CompilerOptions: typeof CompilerOptions
-    PosixFileHandler: typeof PosixFileHandler
-    JsonFileHandler: typeof JsonFileHandler
-}
-
-declare let inkjs: Inkjs
-export = inkjs
-export default inkjs
+export { Story, InkList } from "./engine/Story";
+export { Compiler } from "./compiler/Compiler";
+export { CompilerOptions } from "./compiler/CompilerOptions";
+export { PosixFileHandler } from "./compiler/FileHandler/PosixFileHandler";
+export { JsonFileHandler } from "./compiler/FileHandler/JsonFileHandler";
